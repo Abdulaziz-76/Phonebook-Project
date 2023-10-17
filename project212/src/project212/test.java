b4b7f94a2831034a8c74d55dc96566fad5ad3e5a
Abdulaziz-76 <147327698+Abdulaziz-76@users.noreply.github.com>
2023-10-18 00:03:29 +0300

    Update test.java
    last Update

--- project212/src/project212/test.java
+++ project212/src/project212/test.java
@@ -1,78 +1,72 @@
 package project212;
 
 import java.util.Scanner;
+/*
+CLASS: Test
+CSC212 Data structures - Project phase I
+Fall 2023
+EDIT DATE:
+10-17-2023
+TEAM:
+Abdalaziz Almutairi
+Ibrahim Althanyyan
+Abdullah Alomran
+AUTHORS:
+Abdalaziz Almutairi (443101720)
+Ibrahim Althanyyan  (443101693)
+Abdullah Alomran    (443100868)
+*/
 
 public class test {
-	
-	
+
 	public static void main(String[] args) {
-		int num=0;//number of choice
+		int choice = 0;// number of choice
 		Scanner input = new Scanner(System.in);
 		Phonebook ph = new Phonebook();
 		System.out.println("Welcome to the Linked Tree Phonebook!");
-		
 		do {
-		System.out.println("Please choose an option:"); 
-		System.out.println("1. Add a contact");
-		System.out.println("2. Search for a contact");
-		System.out.println("3. Delete a contact");
-		System.out.println("4. Schedule an event");
-		System.out.println("5. Print event details");
-		System.out.println("6. Print contacts by first name");
-		System.out.println("7. Print all events alphabetically");
-		System.out.println("8. Exit");
-		System.out.println("Enter your choice:");
-		
-		try {
-		 num = input.nextInt();
-		
-		
-		switch(num) {
-		
-		case 1:
-			ph.add_contact(input);
-			break;
-			
-		case 2:
-			ph.search_Contact(input);
-			break;
-			
-		case 3:
-			ph.deleteContact();
-			break;
-			
-		case 4:
-			ph.Schedule(input);
-			break;
-			
-		case 5:
-			ph.searchEvent(input);
-			break;
-			
-		case 6:
-			ph.printBy_firstName(input);
-			break;
-			
-		case 7:
-			ph.lnEvent.printAll();
-			break;
-			
-		case 8:
-			System.out.println("Goodbye!");
-			break;
-			
-			
-		default:
-			System.out.println("Choose a number from 1-8");
-		}
-		}catch(Exception x) {
-		System.err.println("only integers number from 1-8");
-			 input.nextLine();
-		}
-		}while(num!=8);
-		
+
+			try {
+				choice = ph.menu(input);
+
+				switch (choice) {
+				case 1:
+					ph.add_contact(input);
+					break;
+				case 2:
+					ph.search_Contact(input);
+					break;
+				case 3:
+					ph.deleteContact();
+
+					break;
+				case 4:
+					ph.Schedule(input);
+
+					break;
+				case 5:
+					ph.searchEvent(input);
+
+					break;
+				case 6:
+					ph.printBy_firstName(input);
+					break;
+				case 7:
+					ph.lnEvent.printAll();
+					break;
+				case 8:
+					System.out.println("Goodbye!");
+					break;
+
+				default:
+					System.out.println("Choose a number from 1-8");
+				}
+			} catch (Exception x) {
+				System.err.println("only integers number from 1-8");
+				input.nextLine();
+			}
+		} while (choice != 8);
+
 	}
-	//
 }
-	
 
