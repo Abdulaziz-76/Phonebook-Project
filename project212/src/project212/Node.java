5198c7837896e622d98b74a396e4871f06eae00e
Abdulaziz-76 <147327698+Abdulaziz-76@users.noreply.github.com>
2023-10-18 00:10:12 +0300

    Update Node.java
    last Update Node.java

--- project212/src/project212/Node.java
+++ project212/src/project212/Node.java
@@ -1,29 +1,45 @@
 package project212;
+/*
+CLASS: Node
+CSC212 Data structures - Project phase I
+Fall 2023
+EDIT DATE:
+10-17-2023
+TEAM:
+Abdalaziz Almutairi
+Ibrahim Althanyyan
+Abdullah Alomran
+AUTHORS:
+Abdalaziz Almutairi (443101720)
+Ibrahim Althanyyan  (443101693)
+Abdullah Alomran    (443100868)
+*/
+public class Node<T> {
+	private T data;
+	private Node<T> next;
 
-public class Node <T> {
-	public T data;
-	public Node<T> next;
 	public Node() {
-		data =null;
-		next=null;
+
 	}
-	public Node(T val) {
-		data =val;
-		next=null;
+
+	public Node(T d) {
+		this.data = d;
+
 	}
-	public void setData(T data) {
-		this.data = data;
-	}
-	public void setNext(Node<T> next) {
-		this.next = next;
-	}
+
 	public T getData() {
 		return data;
 	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
 	public Node<T> getNext() {
 		return next;
 	}
-//
+
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
 }
-
-
